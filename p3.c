6fde614094c454232ff3bda3591d509f44b96127
Juan Arteaga <juan.arteaga.lasluisa@udla.edu.ec>
2026-06-09 12:08:28 -0500

    feat: agregar estadisticas finales de alumnosy correccion del anterior commit

--- p3.c
+++ p3.c
@@ -17,21 +17,25 @@ typedef struct {
  
 void cargarAlumno(Alumno *a);
 void mostrarAlumno(Alumno *a);
+void buscarAlumno(Alumno alumnos[], int n, char nombreBuscado[]);
+void estadisticas(Alumno alumnos[], int n);
  
 int main() {
     Alumno alumnos[MAX];
+    char nombreBuscado[50];
  
     for (int i = 0; i < MAX; i++) {
         printf("\n--- Alumno %d ---\n", i + 1);
         cargarAlumno(&alumnos[i]);
     }
  
-    printf("\n===== LISTA DE ALUMNOS =====\n");
+    printf("\n===== BUSQUEDA =====\n");
+    printf("Ingrese nombre a buscar: ");
+    scanf("%49s", nombreBuscado);
+    buscarAlumno(alumnos, MAX, nombreBuscado);
  
-    for (int i = 0; i < MAX; i++) {
-        printf("\n--- Alumno %d ---\n", i + 1);
-        mostrarAlumno(&alumnos[i]);
-    }
+    printf("\n===== ESTADISTICAS =====\n");
+    estadisticas(alumnos, MAX);
  
     return 0;
 }
@@ -54,9 +58,43 @@ void cargarAlumno(Alumno *a) {
 }
  
 void mostrarAlumno(Alumno *a) {
+    printf("\n--- ALUMNO ---\n");
     printf("Nombre: %s\n", a->nombre);
     printf("Edad: %d\n", a->edad);
     printf("Calle Principal: %s\n", a->direccion.callePrincipal);
     printf("Numero de Casa: %d\n", a->direccion.numeroCasa);
     printf("Calle Secundaria: %s\n", a->direccion.calleSecundaria);
+}
+ 
+void buscarAlumno(Alumno alumnos[], int n, char nombreBuscado[]) {
+    int i;
+ 
+    for (i = 0; i < n; i++) {
+        if (alumnos[i].nombre[0] == nombreBuscado[0]) {
+            mostrarAlumno(&alumnos[i]);
+            return;
+        }
+    }
+ 
+    printf("Alumno no encontrado.\n");
+}
+ 
+void estadisticas(Alumno alumnos[], int n) {
+    int suma = 0;
+    int mayores = 0;
+    float promedio;
+    int i;
+ 
+    for (i = 0; i < n; i++) {
+        suma += alumnos[i].edad;
+ 
+        if (alumnos[i].edad >= 18) {
+            mayores++;
+        }
+    }
+ 
+    promedio = (float)suma / n;
+ 
+    printf("Promedio de edades: %.2f\n", promedio);
+    printf("Alumnos mayores de edad: %d\n", mayores);
 }
