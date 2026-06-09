61cf13d1dfaa78a41ba0b09440701b2c8597ea0e
Juan Arteaga <juan.arteaga.lasluisa@udla.edu.ec>
2026-06-09 11:44:17 -0500

    feat: agregar funcion para cargar datos del alumno

--- p3.c
+++ p3.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-
+ 
 typedef struct Direccion {
     char callePrincipal[50];
     int numeroCasa;
     char calleSecundaria[50];
 } Direccion;
-
+ 
 typedef struct {
     char nombre[50];
     int edad;
     Direccion direccion;
 } Alumno;
-
+ 
+void cargarAlumno(Alumno *a);
 void mostrarAlumno(Alumno *a);
-
+ 
 int main() {
     Alumno p1;
-
-    strcpy(p1.nombre, "Juan");
-    p1.edad = 20;
-
-    strcpy(p1.direccion.callePrincipal, "Av. Amazonas");
-    p1.direccion.numeroCasa = 123;
-    strcpy(p1.direccion.calleSecundaria, "Naciones Unidas");
-
+ 
+    cargarAlumno(&p1);
     mostrarAlumno(&p1);
-
+ 
     return 0;
 }
-
+ 
+void cargarAlumno(Alumno *a) {
+    printf("Ingrese nombre: ");
+    scanf("%49s", a->nombre);
+ 
+    printf("Ingrese edad: ");
+    scanf("%d", &a->edad);
+ 
+    printf("Ingrese calle principal: ");
+    scanf("%49s", a->direccion.callePrincipal);
+ 
+    printf("Ingrese numero de casa: ");
+    scanf("%d", &a->direccion.numeroCasa);
+ 
+    printf("Ingrese calle secundaria: ");
+    scanf("%49s", a->direccion.calleSecundaria);
+}
+ 
 void mostrarAlumno(Alumno *a) {
+    printf("\n--- DATOS DEL ALUMNO ---\n");
     printf("Nombre: %s\n", a->nombre);
     printf("Edad: %d\n", a->edad);
     printf("Calle Principal: %s\n", a->direccion.callePrincipal);
