ccc53fcbc101637d9d76f7d906400d2d46630a9e
Juan Arteaga <juan.arteaga.lasluisa@udla.edu.ec>
2026-06-09 11:49:50 -0500

    feat: agregar manejo de multiples alumnos con arreglos

--- p3.c
+++ p3.c
@@ -1,59 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-
+ 
+#define MAX 3
+ 
 typedef struct Direccion {
     char callePrincipal[50];
     int numeroCasa;
     char calleSecundaria[50];
 } Direccion;
-
+ 
 typedef struct {
     char nombre[50];
     int edad;
     Direccion direccion;
 } Alumno;
-
+ 
 void cargarAlumno(Alumno *a);
 void mostrarAlumno(Alumno *a);
-void cumplirAnios(Alumno *a);
-
+ 
 int main() {
-    Alumno p1;
-
-    cargarAlumno(&p1);
-
-    printf("\nDatos antes de cumplir anios:\n");
-    mostrarAlumno(&p1);
-
-    cumplirAnios(&p1);
-
-    printf("\nDatos despues de cumplir anios:\n");
-    mostrarAlumno(&p1);
-
+    Alumno alumnos[MAX];
+ 
+    for (int i = 0; i < MAX; i++) {
+        printf("\n--- Alumno %d ---\n", i + 1);
+        cargarAlumno(&alumnos[i]);
+    }
+ 
+    printf("\n===== LISTA DE ALUMNOS =====\n");
+ 
+    for (int i = 0; i < MAX; i++) {
+        printf("\n--- Alumno %d ---\n", i + 1);
+        mostrarAlumno(&alumnos[i]);
+    }
+ 
     return 0;
 }
-
+ 
 void cargarAlumno(Alumno *a) {
     printf("Ingrese nombre: ");
     scanf("%49s", a->nombre);
-
+ 
     printf("Ingrese edad: ");
     scanf("%d", &a->edad);
-
+ 
     printf("Ingrese calle principal: ");
     scanf("%49s", a->direccion.callePrincipal);
-
+ 
     printf("Ingrese numero de casa: ");
     scanf("%d", &a->direccion.numeroCasa);
-
+ 
     printf("Ingrese calle secundaria: ");
     scanf("%49s", a->direccion.calleSecundaria);
 }
-
-void cumplirAnios(Alumno *a) {
-    a->edad++;
-}
-
+ 
 void mostrarAlumno(Alumno *a) {
     printf("Nombre: %s\n", a->nombre);
     printf("Edad: %d\n", a->edad);
